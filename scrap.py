7f373e977a3a5455b367386b981ac8c27eb0a603
Metaroix <ahmadelyakubu@gmail.com>
2025-11-04 15:17:15 +0100

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,6 +1,7 @@
 import keyboard 
 def main():
     print (" hello nigas")
+    print ("NIC NICE")
 keyboard.add_hotkey('a', lambda: keyboard.write('Geek')) 
 keyboard.add_hotkey('ctrl + shift + a', main) 
 
